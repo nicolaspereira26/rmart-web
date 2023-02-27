bf3c4268fdade58a9efa2ffca2baf008bad5d8aa
Nicolas Pereira <nicolas.pereira.vidal@gmail.com>
2023-02-26 23:55:20 -0300

    Inicio em react 	modified:   .vscode/settings.json 	new file:   React/my-app/.gitignore 	new file:   React/my-app/README.md 	new file:   React/my-app/package-lock.json 	new file:   React/my-app/package.json new file:   React/my-app/public/favicon.ico 	new file:   React/my-app/public/manifest.json 	new file:   React/my-app/src/App.js 	renamed:    style.css -> React/my-app/src/Componentes/index.css 	renamed:    index.html -> React/my-app/src/Componentes/index.jsx renamed:    login.css -> React/my-app/src/Componentes/login.css 	new file:   React/my-app/src/index.js 	renamed:    login.html -> React/my-app/src/login.html  new file:   React/my-app/src/logo.svg 	new file:   React/my-app/src/reportWebVitals.js 	new file:   React/my-app/src/setupTests.js

--- React/my-app/src/Componentes/index.jsx
+++ React/my-app/src/Componentes/index.jsx
@@ -3,12 +3,13 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="../src/index.css">
   <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@500&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
   <title>Site</title>
 </head>
 <body>
+  <div class="Cards"></div>
   <div class="category">
     <p>Categorias</p>
   </div>
